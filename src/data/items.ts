aa6f9130bd39480dd51816b88a8cecd0a7eb85d9
Matheus Monteiro <matheus_wellyso2015@hotmail.com>
2023-01-09 15:05:14 -0300

    first commit

---
+++ src/data/items.ts
@@ -0,0 +1,9 @@
+import { Item } from '../types/Item';
+
+export const items: Item[] = [
+	{date: new Date(2023, 1, 2), category: 'food', title: 'McDonalds', value: 32.12 },
+	{date: new Date(2023, 1, 2), category: 'food', title: 'Burger King', value: 32.12 },
+	{date: new Date(2023, 1, 2), category: 'rent', title: 'Aluguel Apt', value: 32.12 },
+	{date: new Date(2023, 2, 3), category: 'salary', title: 'Salário ACM', value: 32.12 },
+
+];
